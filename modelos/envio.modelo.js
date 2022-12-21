fa853ac30c0b4d7e70c907292498229e786cbf66
Oscar Lopez <57050336+keyle0015@users.noreply.github.com>
2022-12-20 22:54:19 -0700

    pagina de error convertida a ejs y otras cosas

---
+++ modelos/envio.modelo.js
@@ -0,0 +1,10 @@
+class envio {
+    constructor(clave, fecha, estado, id) {
+        this.clave = clave;
+        this.fecha = fecha;
+        this.estado = estado;
+        this.id = id;
+    }
+};
+
+export default envio;
