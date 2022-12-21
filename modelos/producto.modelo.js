cc38dad9e7b28c4ffd6b3911f9b33241647a1687
Humberto Barrera <humbertobarcam@gmail.com>
2022-12-20 22:43:18 -0700

    catalogo dinamico

---
+++ modelos/producto.modelo.js
@@ -0,0 +1,16 @@
+class Producto{
+    constructor(id, nombre, autor, editorial, descripcion, precio, tipoPasta, genero, cantidad, portada){
+        this.id = id;
+        this.nombre = nombre;
+        this.autor = autor;
+        this.editorial = editorial;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.tipoPasta = tipoPasta;
+        this.genero = genero;
+        this.cantidad = cantidad;
+        this.portada = portada;
+    }
+};
+
+export default Producto;
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express, { response } from 'express';
 import path from 'path';
 import session from 'express-session';
 import fs from 'fs'
+import {pool} from './db.js'
 import { fileURLToPath } from 'url';
 import clientesRoutes from './routes/clientes.routes.js';
 import productosRoutes from './routes/productos.routers.js';
@@ -18,8 +19,9 @@ const __dirname = path.dirname(path.dirname(__filename));
 
 app.use(express.static(path.join(__dirname, 'styles')));
 // app.use(express.static(path.join(__dirname, 'shared')));
-app.use(express.static(path.join(__dirname, 'img')));
+app.use(express.static(path.join(__dirname, 'views/img')));
 app.use(express.static(path.join(__dirname, 'js')));
+app.use(express.static(path.join(__dirname, 'views/contents/portadas')));
 // app.use(express.static(path.join(__dirname, 'contents')));
 
 // Sesion
@@ -43,24 +45,6 @@ app.use('/api', ordenRoutes);
 
 
 // Vistas
-// Menú principal
-app.get('/homeasd', function (req, response) {
-	// Render login template
-	if (req.session.loggedIn) {
-		const $ = cheerio.load(fs.readFileSync(__dirname + '/shared/nav.html'))
-		const perfil = $('<a class="login" href="/micuenta">Mi perfil</a>')
-		$('.login').replaceWith(perfil)
-		response.sendFile(path.join(__dirname + '/index.html'));
-	} else {
-		response.sendFile(path.join(__dirname + '/index.html'));
-	}
-});
-
-// Iniciar Sesion
-app.get('/iniciar_sesion2', function (req, response) {
-	// Render login template
-	response.sendFile(path.join(__dirname + '/contents/login.html'));
-});
 
 app.get('/salir', function (req, response) {
 	// Render login template
@@ -68,84 +52,11 @@ app.get('/salir', function (req, response) {
 	response.redirect('/');
 });
 
-// Mi cuenta
-app.get('/micuentsda', function (req, response) {
-	// Render login template
-	if (req.session.loggedIn) {
-		const $ = cheerio.load(fs.readFileSync(__dirname + '/contents/cuenta.html'))
-		$('.contenido h1').replaceWith(`<h1>${req.session.user}</h1>`)
-		$('.contenido p:contains("John Doe")').replaceWith(`<p>${req.session.user}</p>`)
-		$('.contenido p:contains("jondoe@email.com")').replaceWith(`<p>${req.session.email}</p>`)
-		$('.contenido.dir p').replaceWith(`<p>${req.session.direccion}</p>`)
-		if (req.session.instrucciones_direccion != null) {
-			$('#insEntrega p').replaceWith(`<p>${req.session.instrucciones_direccion}</p>`)
-		} else {
-			$('#insEntrega p').replaceWith(`<p></p>`)
-		}
-		$('#nombreTarjeta p').replaceWith(`<p>${req.session.nombreTarjeta}</p>`)
-		$('#numTarjeta p').replaceWith(`<p>${req.session.tarjeta}</p>`)
-		$('#venc').replaceWith(`<p>${req.session.fechaVenTarjeta}</p>`)
-		// console.log(req.session.fechaVen)
-		response.send($.html());
-	} else {
-		response.sendFile(path.join(__dirname + '/contents/login.html'));
-	}
-});
-
-// Carrito
-app.get('/micarrito', function (req, response) {
-	if (req.session.loggedIn) {
-		const $ = cheerio.load(fs.readFileSync(__dirname + '/contents/micarrito.html'))
-		$("#carritoBod").html('')
-		console.log(req.session.carrito)
-		for (let i = 0; i <= req.session.carrito; i++) {
-			var e = $(`<tr> +
-				< th scope = "row" >  
-				<div class="multi">
-					<img class="cover" src="${req.session.pedidoNom[i]}.jpg" alt=""> +
-				<p class="titulo">${req.session.pedidoNom[i]}</p>  +
-				</div>
-					</th >
-					<td><p class="precio">${req.session.pedidoPre[i]}</p></td>
-					<td class="cant">
-						<div class="multi">
-							<button class="qButton">-</button>
-							<input type="number" value="${req.session.pedidoCant[i]}">
-							<button class="qButton">+</button>
-						</div>
-					</td>
-					<td><button class="brl-butt"><img src="/trash.svg" alt=""></button></td>`)
-			$("#carritoBod").append(e)
-		response.send($.html())
-		}
-	} else {
-		response.sendFile(path.join(__dirname + '/contents/micarrito.html'));
-	}
-});
-
 // Pedidos
 app.get('/mispedidos', function (req, response) {
 	// Render login template
 	response.sendFile(path.join(__dirname + '/contents/pedidos.html'));
 });
-
-// Catálogo
-app.get('/catalogo', function (req, response) {
-	// Render login template
-	response.sendFile(path.join(__dirname + '/contents/catalogo.html'));
-});
-
-// Catálogo
-app.get('/mispagos', function (req, response) {
-	// Render login template
-	response.sendFile(path.join(__dirname + '/contents/mispagos.html'));
-});
-
-app.get('/ventas', function (req, response) {
-	// Render login template
-	response.sendFile(path.join(__dirname + '/contents/ventas.html'));
-});
-
 
 // Pruebas para renderizar las vistas de otra forma
 
@@ -174,6 +85,14 @@ app.get('/micuenta', (req, res) => {
 	}else{
 		res.render('login', {userLogged: req.session.loggedIn, user: req.session.user})
 	}
+});
+
+// Catálogo
+app.get('/catalogo', (req, res) => {
+	pool.execute('SELECT * FROM producto').then(([data, fields]) => {
+		console.log(data)
+		res.render('catalogo', {userLogged: req.session.loggedIn, user: req.session.user, productos: data});
+	})
 });
 
 app.use((req, res, next) => {
