cf2210c81d3ff443e5d2c77e9cce3581345ab801
Humberto Barrera <humbertobarcam@gmail.com>
2023-01-05 14:47:32 -0700

    servicio de venta 95% falta envios

--- src/routes/ventas.routes.js
+++ src/routes/ventas.routes.js
@@ -5,6 +5,7 @@ import { createVenta, getVenta } from "../controllers/ventas.controller.js";
 const router = Router()
 
 router.get('/ventas', getVenta)
+
 router.post('/ventas', createVenta)
 
 export default router
